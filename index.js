e8d97f10a67691c290b5c2ba16ad17d0879ea5d2
singhmanmeetsingh <singhmanmeet523@gmail.com>
2023-05-24 22:04:28 -0400

    errors handled

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-
+recognition.continuous = true;
+recognition.interimResults = true;
 const start = () => {
   recognition.start();
 };
 
 
+recognition.onerror = function (event) {
+  // Handle the error
+  switch (event.error) {
+    case "network":
+      console.error("There was a network error");
+      break;
+    case "not-allowed":
+      console.error("The user has denied permission to use the microphone");
+      break;
+    case "service-not-allowed":
+      console.error(
+        "The user has denied permission to use the speech recognition service"
+      );
+      break;
+    default:
+      console.error(event.error);
+  }
+};
+
 
 recognition.onspeechend = function () {
-//   recognition.start();
     recognition.stop();
 };
 
 recognition.onresult = (event) => {
     const text = event.results[0][0].transcript;
     console.log("text", text)
+    alert(text)
 };
 
